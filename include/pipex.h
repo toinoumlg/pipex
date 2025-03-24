142c3f084f8e4c78f97e507a4352730547447b06
toinoumlg <antoine.malanguerray@gmail.com>
2025-03-24 20:25:22 +0100

    started checking

--- include/pipex.h
+++ include/pipex.h
@@ -6,13 +6,23 @@
 /*   By: amalangu <amalangu@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/23 18:11:46 by amalangu          #+#    #+#             */
-/*   Updated: 2025/03/23 18:13:16 by amalangu         ###   ########.fr       */
+/*   Updated: 2025/03/24 18:31:16 by amalangu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
-#define PIPEX_H
+# define PIPEX_H
 
-#include "../libftprintf/include/libftprintf.h"
+# include "../libftprintf/include/libftprintf.h"
+# include <fcntl.h>
+
+typedef struct s_pipex
+{
+	char	**env;
+	char	***args;
+	int		f1;
+	int		f2;
+
+}			t_pipex;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,13 +6,66 @@
 /*   By: amalangu <amalangu@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/23 18:09:16 by amalangu          #+#    #+#             */
-/*   Updated: 2025/03/23 18:25:48 by amalangu         ###   ########.fr       */
+/*   Updated: 2025/03/24 18:55:21 by amalangu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/pipex.h"
 
-int	main(void)
+char	**set_env(char **envp)
 {
-	ft_printf("CACA");
+	char	**env;
+	int		i;
+
+	i = -1;
+	env = ft_split(ft_strchr(envp[4], '/'), ':');
+	if (!env)
+		return (NULL);
+	while (env[i++])
+		env[i] = ft_strjoin(env[i], "/");
+	return (env);
+}
+
+int	open_fds(int ac, char **av, t_pipex *pipex)
+{
+	pipex->f1 = open(av[1], O_RDWR);
+	pipex->f2 = open(av[ac - 1], O_RDWR);
+	if (pipex->f1 < 0 && pipex->f2 < 0)
+		return (-1);
+	else if (pipex->f1 < 0)
+		return (close(pipex->f2), -1);
+	else if (pipex->f2 < 0)
+		return (close(pipex->f1), -1);
+	else
+		return (0);
+}
+
+int	set_args(int ac, char **av, t_pipex *pipex)
+{
+	
+}
+
+int	set_args(int ac, char **av, t_pipex *pipex)
+{
+	if (ac < 2)
+		return (-1);
+	if (open_fds(ac, av, pipex))
+		return (-1);
+	if (set_cmds(ac, av, pipex))
+}
+
+int	check_args(int ac, char **av, char **envp, t_pipex *pipex)
+{
+	pipex->env = set_env(envp);
+	if (set_args(ac, av, pipex))
+		return (-1);
+	ft_printf("%s\n", pipex->env[0]);
+	return (0);
+}
+
+int	main(int ac, char **av, char **envp)
+{
+	t_pipex pipex;
+	if (check_args(ac, av, envp, &pipex))
+		return (-1);
 }
